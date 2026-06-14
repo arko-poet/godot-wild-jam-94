b6d5e68055b176071edce4627960df2de6406398
arko <arkadiusz.kupracz@gmail.com>
2026-06-14 13:23:56 +0100

    add first iteration of genetic algorithm

--- game/dna/dna_strand.gd
+++ game/dna/dna_strand.gd
@@ -2,6 +2,63 @@ class_name DNAStrand extends RefCounted
 
 var bases: Array[DNABase]
 
+## modifies the bases of this DNAStrand by the incoming_strand
+##
 ## position: where the beginning of the incoming strand is positioned relative to the this DNAStrand
-func combine_strands(strand: DNAStrand, position: int) -> void:
-	pass
+##
+## Genetic Operations:
+## 1. Conservation = no aligned base from incoming strand so base does not change 
+## 2. Evolution = bases match so base value is added
+## 3. Mutation = bases don't match so random change occurs
+## 4. Growth = 1 incoming base is added if it has no pair in this DNAStrand
+## 5. Rejection = remaining incoming DNA bases get rejected
+func combine_strands(incoming_strand: DNAStrand, position: int, mutation_allowed: bool = false) -> void:
+	assert(incoming_strand.bases.size() > 0)
+	assert(position < bases.size())
+	assert(incoming_strand.bases.size() + position > 0)
+
+	var incoming_strand_index: int
+	if position < 0:
+		incoming_strand_index = position * -1
+	else:
+		incoming_strand_index = 0
+	
+	var new_bases: Array[DNABase]
+	for base_index in bases.size():
+		var base = bases[base_index]
+		
+		# Conservation
+		if incoming_strand_index > base_index or incoming_strand_index >= incoming_strand.bases.size(): 
+			new_bases.append(base)
+		elif position < incoming_strand.bases.size():
+			var incoming_base = incoming_strand.bases[incoming_strand_index]
+			# Evolution
+			if base.is_matching(incoming_base):
+				base.value += incoming_base.value
+				new_bases.append(base)
+			# Mutation
+			elif mutation_allowed:
+				if randf() < 0.5:
+					incoming_base.value -= base.value
+					base = incoming_base
+					new_bases.append(base)
+				else:
+					base.value -= incoming_base.value
+			# Conservation
+			else:
+				new_bases.append(base)
+		else:
+			new_bases.append(base)
+		
+		new_bases.append(base)
+		if incoming_strand_index == base_index:
+			incoming_strand_index += 1
+	
+	# Growth
+	if incoming_strand_index < incoming_strand.bases.size():
+		new_bases.append(incoming_strand.bases[incoming_strand_index])
+	
+	# Rejection
+	# Remaining bases in incroming strand get ignored
+			
+	bases = new_bases

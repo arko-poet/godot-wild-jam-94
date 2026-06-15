269c9b38974246700774d910ad9c4661ceb4de57
Mansoor Faqiri <mzfaqiri@gmail.com>
2026-06-15 16:24:41 -0400

    added log and stats icons

--- game/autobattler/autobattler.gd
+++ game/autobattler/autobattler.gd
@@ -13,9 +13,20 @@ func find_party_members(parent: Node, pattern: String) -> Array[PartyMember]:
 @onready var ally_party: Array[PartyMember] = find_party_members(%AllyPartyMembers, "PartyMember")
 @onready var turn_order: Array[PartyMember] = enemy_party + ally_party
 
+@onready var ally_stats: Stats = %AllyStats
+@onready var enemy_stats: Stats = %EnemyStats
+
 func _ready() -> void:
 	print("party members in turn order")
 	print(turn_order.size())
+	ally_stats.init_stats(ally_party[0].strength
+	,ally_party[0].health
+	,ally_party[0].speed)
+
+	enemy_stats.init_stats(enemy_party[0].strength
+	,enemy_party[0].health
+	,enemy_party[0].speed)
+
 	_auto_battle()
 
 func _auto_battle() -> void:
@@ -30,10 +41,22 @@ func _auto_battle() -> void:
 				continue
 
 			var target_player: PartyMember
+			var target_stats: Stats
 			if party_member in ally_party:
 				target_player = enemy_party[0]
+				target_stats = enemy_stats
+				if target_player.dead:
+					enemy_party.erase(target_player)
+					turn_order.erase(target_player)
+					continue
 			else:
 				target_player = ally_party[0]
+				target_stats = ally_stats
+				if target_player.dead:
+					ally_party.erase(target_player)
+					turn_order.erase(target_player)
+					continue
+
 
 			party_member.target_player = target_player
-			party_member.do_turn()
+			party_member.do_turn(%CombatLog, target_stats)
--- game/autobattler/party_member.gd
+++ game/autobattler/party_member.gd
@@ -5,33 +5,36 @@ class_name PartyMember extends TextureRect
 @export var MAX_STRENGTH: int
 @export var member_name: String
 
-@onready var speed = MAX_SPEED
-@onready var health = MAX_HEALTH
-@onready var strength = MAX_STRENGTH
+@onready var speed: int = MAX_SPEED
+@onready var health: int = MAX_HEALTH
+@onready var strength: int = MAX_STRENGTH
 var target_player: PartyMember
 var dead: bool = false
 
 func _ready() -> void:
 	pass
 
-func do_turn() -> void:
+func do_turn(combat_log: RichTextLabel, target_stats: Stats) -> void:
 	print("doing turn")
-	print(target_player)
-	target_player.take_dmg(strength)
-	print("speed")
-	print(speed)
+	var combat_text = "{0} Does {1} [color=orange][b]DAMAGE[/b][/color]".format([member_name, strength])
+
+	combat_log.append_text(combat_text)
+	combat_log.newline()
+	target_player.take_dmg(strength, combat_log, target_stats)
 	pass
 
-func take_dmg(dmg: int) -> void:
+func take_dmg(dmg: int, combat_log: RichTextLabel, target_stats: Stats) -> void:
 	health -= dmg
 	print("took damage")
-	print(health)
+	target_stats.update_HP(health)
 	if health <= 0:
-		handle_death()
+		handle_death(combat_log)
 	pass
 
-func handle_death() -> void:
+func handle_death(combat_log: RichTextLabel) -> void:
 	dead = true
+	combat_log.append_text("{0} [color=red][b]DIES[/b][/color]".format([member_name]))
+	combat_log.newline()
 	print("died")
-	self.visible = false
+	#self.visible = false
 	pass

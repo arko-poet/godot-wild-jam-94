ff43894f4d7f57d1637b80b1b9b7274b7dd5ade9
arko <arkadiusz.kupracz@gmail.com>
2026-06-15 18:24:51 +0100

    add DNA strand drawing

---
+++ game/dna/dna_strand_drawing/dna_strand_drawing.gd
@@ -0,0 +1,33 @@
+class_name DnaStrandDrawing extends Node2D
+
+const BASE_HEIGHT := 16
+const BASE_SPACING := 8
+const BASE_SHAPE := Vector2(32, 16)
+
+var strand: DNAStrand
+var shift: int ## how to shift the strand up and down
+
+@export var is_left := true ## otherwise it's right
+
+func _ready() -> void:
+	_test_strand() # TODO delete after testing
+	queue_redraw()
+
+
+func _draw() -> void:
+	var strand_height = BASE_HEIGHT * strand.bases.size() + BASE_SPACING * (strand.bases.size() - 1)
+	draw_line(Vector2.ZERO, Vector2(0, strand_height), Color.BLACK, 4)
+	
+	var base_position := Vector2(4, 0)
+	if not is_left: base_position.x = base_position.x * -1 - BASE_SHAPE.x
+	for base in strand.bases:
+		var base_rect := Rect2(base_position, BASE_SHAPE)
+		draw_rect(base_rect, Color.HOT_PINK)
+		base_position.y += BASE_HEIGHT + BASE_SPACING
+
+
+func _test_strand() -> void:
+	strand = DNAStrand.new()
+	for i in 8:
+		strand.bases.append(DNABase.new(DNABase.Shape.RoundSocket, DNABase.Attribute.HEALTH, 4))
+		
--- game/dna/mutation_screen/mutation_screen.gd
+++ game/dna/mutation_screen/mutation_screen.gd
@@ -3,29 +3,31 @@ extends Node
 signal mutation_finished
 
 @onready var player_creature: Sprite2D = $World/PlayerCreature
-@onready var defeated_creature: Sprite2D = $World/DefeatedCreature
+@onready var corpse: Sprite2D = $World/Corpse
+@onready var strand_a_drawing: DnaStrandDrawing = $World/StrandADrawing
+@onready var strand_b_drawing: DnaStrandDrawing = $World/StrandBDrawing
+
 
 @onready var confirm_mutation_button: Button = $UILayer/UI/ConfirmMutationButton
 @onready var skip_mutation_button: Button = $UILayer/UI/SkipMutationButton
 @onready var shift_strand_up_button: Button = $UILayer/UI/ShiftStrandUpButton
 @onready var shift_strand_down_button: Button = $UILayer/UI/ShiftStrandDownButton
 
-var strandA: DNAStrand
-var strandB: DNAStrand
-var strandB_position: int ## relative to strandA
-
 
 ## TODO change parameters to get creature objects - for adding sprites etc.
 ## strandA -> player creature's strand that will change
 ## strandB -> slain creature strand that will affect strandA
-func load_dna_strands(p_strandA: DNAStrand, p_strandB: DNAStrand) -> void:
-	strandB_position = 0
-	strandA = p_strandA
-	strandB = p_strandB
+func load_dna_strands(strandA: DNAStrand, strandB: DNAStrand) -> void:
+	strand_a_drawing.strand = strandA
+	strand_b_drawing.strand = strandB
+	strand_b_drawing.shift = 0
+	strand_b_drawing.is_left = false
+	strand_a_drawing.queue_redraw()
+	strand_b_drawing.queue_redraw()
 
 
 func _on_confirm_mutation_button_pressed() -> void:
-	strandA.combine_strands(strandB, strandB_position)
+	strand_a_drawing.strand.combine_strands(strand_b_drawing.strand, strand_b_drawing.shift)
 	mutation_finished.emit()
 
 
@@ -34,8 +36,8 @@ func _on_skip_mutation_button_pressed() -> void:
 
 
 func _on_shift_strand_up_button_pressed() -> void:
-	strandB_position -= 1
+	strand_b_drawing.shift -= 1
 
 
 func _on_shift_strand_down_button_pressed() -> void:
-	strandB_position += 1
+	strand_b_drawing.shift += 1

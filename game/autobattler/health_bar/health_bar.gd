8481fd317378d4720f101c8bb184e9c70bcdc253
arko <arkadiusz.kupracz@gmail.com>
2026-06-18 10:37:06 +0100

    change health bar appearance

--- game/autobattler/health_bar/health_bar.gd
+++ game/autobattler/health_bar/health_bar.gd
@@ -1,5 +1,6 @@
 class_name CreatureDisplay extends TextureProgressBar
 
+@onready var label: Label = $Label
 
 var creature: Creature:
 	set(value):
@@ -11,3 +12,4 @@ var creature: Creature:
 func _update_health() -> void:
 	max_value = creature.max_health
 	value = creature.health
+	label.text = "%s/%s" % [creature.health, creature.max_health]

fe6740cfc55a2bd0366f79b74daad28d1ca565bb
arko <arkadiusz.kupracz@gmail.com>
2026-06-19 19:42:04 +0100

    add turtle name scene

--- game/ui/turtle_name_screen/turtle_name_scene.gd
+++ game/ui/turtle_name_screen/turtle_name_scene.gd
@@ -10,3 +10,11 @@ const DEFAULT_NAME := "Archibald"
 func _on_button_pressed() -> void:
 	var turtle_name = DEFAULT_NAME if line_edit.text.is_empty() else line_edit.text
 	name_chosen.emit(turtle_name)
+
+
+func _on_name_chosen(turtle_name: String) -> void:
+	print(turtle_name)
+
+
+func _on_turtle_pressed() -> void:
+	pass # Replace with function body.

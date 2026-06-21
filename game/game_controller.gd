6431484523b30c3ea7f33ddf15e3e6a002d40a6e
arko <arkadiusz.kupracz@gmail.com>
2026-06-21 05:58:49 +0100

    add cutout version of combat music

--- game/game_controller.gd
+++ game/game_controller.gd
@@ -1,7 +1,7 @@
 class_name GameController extends Node
 
 const MUTATION_MUSIC := preload("res://assets/music/DnaScreen_BPM110_L58B_LOOP.mp3")
-const AUTOBATTLE_MUSIC := preload("res://assets/music/DnaScreenPhase2_WO_Xylophone.mp3")
+const AUTOBATTLE_MUSIC := preload("res://assets/music/DnaScreenPhase2(NoIntro).mp3")
 const BOSS_MUSIC := preload("res://assets/music/Bossfight(NoIntro)_BPM120_L50B.mp3")
 
 const AutobattlerScene := preload("res://game/autobattler/autobattler.tscn")
